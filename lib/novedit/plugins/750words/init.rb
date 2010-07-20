e4d1123fbf25c5cb32c77482eb638ce9d3ac9a80
Henri Bourcereau <henri.bourcereau@gmail.com>
2010-07-20 17:54:05 +0200

    750words in progress

--- lib/novedit/plugins/750words/init.rb
+++ lib/novedit/plugins/750words/init.rb
@@ -5,6 +5,7 @@ NoveditMode.define "750words" do
   site "http://www.rhumbs.fr"
   version "0.2"
 
+  #Update wordcount stats metas in current node
   @update_count = lambda do
     now = DateTime.now
     today = [now.year, now.month, now.day].join('-')
@@ -21,12 +22,21 @@ NoveditMode.define "750words" do
     @plugins_proxy.update_last_metas({'750words' => metas}, @model.current_node)
   end
 
+  def get_day_count(node)
+    count = 0
+    metas = @plugins_proxy.get_metas(['750words', today], node)
+    if metas
+       count = metas.split(',').first.split('=').last.to_i - metas.split(',').last.split('=').last.to_i
+    end
+    return count
+  end
+
   def init_current_node
     now = DateTime.now
     today = [now.year, now.month, now.day].join('-')
     metas = @plugins_proxy.get_metas(['750words', today], @plugins_proxy.model.current_node)
     if metas
-      wordcountini = metas.split(',').first.split('=').first.to_i
+      wordcountini = metas.split(',').first.split('=').last.to_i
     else
       wordcountini = @plugins_proxy.count_view_words
     end
@@ -39,7 +49,10 @@ NoveditMode.define "750words" do
 
     wordcountini = init_current_node
     plugins_proxy.add_status(lambda do
-      wordcount = @plugins_proxy.count_view_words - wordcountini
+      wordcount_total_meta = @plugin_proxy.model.nodes.inject(0) do |sum, node|
+        sum + get_day_count(node)
+      end
+      wordcount = wordcount_total_meta - get_day_count(@plugins_proxy.model.current_node) + @plugins_proxy.count_view_words - wordcountini
       color_code = (wordcount < 750) ? '65535-0-0' : '0-65535-0'
       color = Gdk::Color.new(*color_code.split('-').map{|c| c.to_i})
       status = {'text' => wordcount.to_s, 'position' => 'right', 'color' => color}
